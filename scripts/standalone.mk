2538ba4d6eec768d70d61640c9739c5ad899279a
Fu-Ching Yang <fcyangesl@gmail.com>
2019-04-25 21:40:04 -0700

    Add coremark specific linker script for e20/e21/e24 RTLsim Arty
    E21/E24 has 2 x 32-KB tim. In the original linker script, text is
    at one tim and data is at another.
    Since Coremark's text size exceeds 32 KB, the original linker script
    doesn't work. The solution to put parts of the text in another tim,
    sharing the same tim that data is at.
    
    Note that the hot functions and data are placed at different tim for
    performance reason.

--- scripts/standalone.mk
+++ scripts/standalone.mk
@@ -40,7 +40,13 @@ endif
 ifeq ($(PROGRAM),dhrystone)
 LINK_TARGET = ramrodata
 else ifeq ($(PROGRAM),coremark)
+
+ifneq (,$(filter $(TARGET),coreip-e21-rtl coreip-e24-rtl coreip-e20-arty coreip-e21-arty coreip-e24-arty))
+LINK_TARGET = ramrodata.coremark
+else
 LINK_TARGET = ramrodata
+endif
+
 else
 LINK_TARGET = default
 endif
